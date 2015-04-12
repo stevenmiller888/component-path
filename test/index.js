d5d79e266348d55708d2607b8d53f281be08b6e4
stevenmiller888 <stevenmiller888@me.com>
2015-04-11 21:22:58 -0700

    added test/

---
+++ test/index.js
@@ -0,0 +1,21 @@
+
+/**
+ * Dependencies.
+ */
+
+var assert = require('assert');
+var cssPath = require('..');
+
+/**
+ * Tests.
+ */
+
+describe('cssPath()', function(){
+  it('should be a function', function(){
+    assert.equal(typeof cssPath, 'function');
+  });
+  
+  it('should give the path to an element', function(){
+    // TODO
+  });
+});
